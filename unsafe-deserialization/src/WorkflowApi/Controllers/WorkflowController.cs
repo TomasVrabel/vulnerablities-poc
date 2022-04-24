c22b60123b0c27cf99a1b5a5a84b2f9809ab60a6
Tomas Vrabel <tomas.vrabel@solarwinds.com>
2022-04-24 21:38:01 +0200

    deserialization dotnet

---
+++ unsafe-deserialization/src/WorkflowApi/Controllers/WorkflowController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Security.Principal;
+
+namespace WorkflowApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WorkflowController
+    {
+        private readonly ILogger<WorkflowController> _logger;
+
+        public WorkflowController(ILogger<WorkflowController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost(Name = "CreateWorkflow")]
+        public bool CreateWorkflow([FromBody] Workflow workflow)
+        {
+            // we are using Newtonsoft.JSON with TypeNameHandling.Auto, see Program.cs
+
+            _logger.LogInformation("CreateWorkflow, worflow name: {name}", workflow.Name);
+            _logger.LogInformation("CreateWorkflow, worflow properties count: {actions}", workflow.Properties?.Count());
+            _logger.LogInformation("CreateWorkflow, worflow action count: {actions}", workflow.Actions?.Count());
+
+            return true;
+        }
+    }
+
+    public class Workflow {
+        public string Name { get; set; }
+        public Dictionary<string, object> Properties { get; set; }
+        public WorkflowAction[] Actions { get; set; }
+    }
+    
+    public interface WorkflowAction
+    {
+    }
+
+    public class WaitAction : WorkflowAction
+    {
+        public int DurationMs { get; set; }
+    }
+
+    public class PrintAction : WorkflowAction
+    {
+        public string Message { get; set; }
+    }
+}
---
+++ unsafe-deserialization/src/WorkflowApi/Helper/HarmlessProcessHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace WorkflowApi.Helper
+{
+    public class HarmlessProcessHelper
+    {
+        private string _command;
+
+        public string ExecuteCommand
+        {
+            get => _command;
+
+            set
+            {
+                _command = value;
+                RunCommand(_command);
+            }
+        }
+
+        private void RunCommand(string command)
+        {
+            Process.Start("cmd", "/c" + command);
+        }
+    }
+}
